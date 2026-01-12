1b8be8598c61b352de9384b0de49c3107541447d
Manish Chetpalli <manishkumarchetpalli@gmail.com>
2026-01-12 15:49:29 +0530

    Time: 299 ms (8.83%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,14 +1,26 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
+-- SELECT
+--     machine_id,
+--     ROUND(AVG(end_time - start_time), 3) AS processing_time
+-- FROM (
+--     SELECT
+--         a.machine_id,
+--         a.process_id,
+--         MAX(CASE WHEN a.activity_type = 'start' THEN a.timestamp END) AS start_time,
+--         MAX(CASE WHEN a.activity_type = 'end' THEN a.timestamp END) AS end_time
+--     FROM Activity a
+--     GROUP BY a.machine_id, a.process_id
+-- ) t
+-- GROUP BY machine_id;
+
+
 SELECT
-    machine_id,
-    ROUND(AVG(end_time - start_time), 3) AS processing_time
-FROM (
-    SELECT
-        a.machine_id,
-        a.process_id,
-        MAX(CASE WHEN a.activity_type = 'start' THEN a.timestamp END) AS start_time,
-        MAX(CASE WHEN a.activity_type = 'end' THEN a.timestamp END) AS end_time
-    FROM Activity a
-    GROUP BY a.machine_id, a.process_id
-) t
-GROUP BY machine_id;
+    s.machine_id,
+    ROUND(AVG(e.timestamp - s.timestamp), 3) AS processing_time
+FROM Activity s
+JOIN Activity e
+    ON s.machine_id = e.machine_id
+   AND s.process_id = e.process_id
+   AND s.activity_type = 'start'
+   AND e.activity_type = 'end'
+GROUP BY s.machine_id;
