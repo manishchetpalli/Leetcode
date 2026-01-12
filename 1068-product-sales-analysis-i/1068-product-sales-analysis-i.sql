fefabe02778a4b801cb0f5c514aaebae64c5eff8
Manish Chetpalli <manishkumarchetpalli@gmail.com>
2026-01-12 13:06:46 +0530

    Time: 1450 ms (12.25%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,6 +1,4 @@
-SELECT product_name,
-       year,
-       price
-FROM Sales s
-JOIN Product p
-WHERE s.product_id = p.product_id
+SELECT p.product_name,s.year,s.price
+FROM sales As s
+INNER JOIN product as p
+ON p.product_id=s.product_id
