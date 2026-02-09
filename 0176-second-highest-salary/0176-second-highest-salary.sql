54fb6b7c13071d7f7a66e3db6addcbc84b7081be
Manish Chetpalli <manishkumarchetpalli@gmail.com>
2026-02-09 21:04:20 +0530

    Time: 282 ms (31.84%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,4 @@
-# Write your MySQL query statement below
-SELECT salary AS SecondHighestSalary
+SELECT MAX(salary) AS SecondHighestSalary
 FROM (
     SELECT DISTINCT salary,
            DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
